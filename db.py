48cce8e38ce1e520f180ed9903cb90dd2fd29162
globaldepothub <globaldepothub@gmail.com>
2026-05-27 21:58:59 +0800

    Add files via upload

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import pandas as pd
 engine = create_engine("sqlite:///data/sublet.db")
 
 # =========================
-# INIT TABLE (RUN ON FIRST TIME)
+# INIT TENANTS TABLE
 # =========================
 def init_db():
     df = pd.DataFrame(columns=[
@@ -13,13 +13,38 @@ def init_db():
     df.to_sql("tenants", engine, if_exists="replace", index=False)
 
 # =========================
-# LOAD DATA
+# LOAD TENANTS DATA
 # =========================
 def load_data():
     return pd.read_sql("tenants", engine)
 
 # =========================
-# SAVE DATA
+# SAVE TENANTS DATA
 # =========================
 def save_data(df):
     df.to_sql("tenants", engine, if_exists="replace", index=False)
+
+# =========================
+# INIT EXPENSES TABLE
+# =========================
+def init_expenses():
+    df = pd.DataFrame(columns=[
+        "Date",
+        "House",
+        "Category",
+        "Description",
+        "Amount"
+    ])
+    df.to_sql("expenses", engine, if_exists="replace", index=False)
+
+# =========================
+# LOAD EXPENSES DATA
+# =========================
+def load_expenses():
+    return pd.read_sql("expenses", engine)
+
+# =========================
+# SAVE EXPENSES DATA
+# =========================
+def save_expenses(df):
+    df.to_sql("expenses", engine, if_exists="replace", index=False)
